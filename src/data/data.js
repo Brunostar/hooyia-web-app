1cc84d963f2719231e8694b019a1b313cee15c79
Brunostar <tanjubrunostar0@gmail.com>
2022-12-22 05:54:38 +0100

    Created a data file which contains sample data i can use as i code

---
+++ src/data/data.js
@@ -0,0 +1,108 @@
+const data = {
+    "books" : [
+        {
+            "id": 1,
+            "image": "../img/book1.png",
+            "title": "The upside of uncertainty",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 2,
+            "image": "../img/book2.png",
+            "title": "The Boys from Biloxi",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 3,
+            "image": "../img/book3.png",
+            "title": "The Stolen Child",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 4,
+            "image": "../img/book4.png",
+            "title": "Long Shadows",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        }
+    ],
+    "courses" : [
+        {
+            "id": 1,
+            "image": "../img/book1.png",
+            "title": "The upside of uncertainty",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 2,
+            "image": "../img/book2.png",
+            "title": "The Boys from Biloxi",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 3,
+            "image": "../img/book3.png",
+            "title": "The Stolen Child",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 4,
+            "image": "../img/book4.png",
+            "title": "Long Shadows",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        }
+    ],
+    "skills" : [
+        {
+            "id": 1,
+            "image": "../img/book1.png",
+            "title": "The upside of uncertainty",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 2,
+            "image": "../img/book2.png",
+            "title": "The Boys from Biloxi",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 3,
+            "image": "../img/book3.png",
+            "title": "The Stolen Child",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 4,
+            "image": "../img/book4.png",
+            "title": "Long Shadows",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        }
+    ],
+    "applications" : [
+        {
+            "id": 1,
+            "image": "../img/book1.png",
+            "title": "The upside of uncertainty",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 2,
+            "image": "../img/book2.png",
+            "title": "The Boys from Biloxi",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 3,
+            "image": "../img/book3.png",
+            "title": "The Stolen Child",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        },
+        {
+            "id": 4,
+            "image": "../img/book4.png",
+            "title": "Long Shadows",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rutrum ipsum. Fusce mattis nisi ac elit pulvinar porttitor. Aliquam et tristique leo, ac rutrum diam. Maecenas luctus leo non gravida mattis. Sed sodales bibendum nisl non pharetra. Vivamus laoreet urna nec augue feugiat, sit amet imperdiet turpis laoreet. Quisque in dui suscipit, efficitur lectus nec, sollicitudin diam. Vivamus ullamcorper nulla cursus metus scelerisque viverra sit amet quis odio. In condimentum dictum dapibus. Sed at accumsan urna. Integer dolor nibh, egestas nec est a, lobortis elementum ligula. Curabitur eget tellus a augue viverra scelerisque. Nam in luctus nulla. Fusce tempor imperdiet quam, sit amet rutrum enim tristique nec. Curabitur molestie dui nisl, vel vulputate libero vulputate interdum. Nullam maximus sagittis justo eu semper."
+        }
+    ]
+}
+
+export default data;
